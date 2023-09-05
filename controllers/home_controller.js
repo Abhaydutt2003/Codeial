285c7a90ce05c0e28c149caa3e1b35f4fe4d857a
AbhayDutt2003 <dutt.abhay@gmail.com>
2023-09-05 07:37:10 +0530

    learn to delete post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,21 @@
 const Post = require('../models/post');
 
 module.exports.home = function (request, response) {
-    Post.find({}).populate('user').exec().then(function(posts){
-        return response.render('home',{
-            title:"Codeial | Home",
-            posts:posts
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         })
-    }).catch(function(error){
-        console.log("Error in finding the user || home.controller");
-        return;
-    }) 
+        .exec().then(function (posts) {
+            return response.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            })
+        }).catch(function (error) {
+            console.log("Error in finding the user || home.controller");
+            return;
+        })
 }
--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -2,15 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
-module.exports.create = function(request,response){
+module.exports.create = function (request, response) {
     Post.create({
-        content:request.body.content,
-        user:request.user._id
-    }).then(function(Post){
+        content: request.body.content,
+        user: request.user._id
+    }).then(function (Post) {
         return response.redirect('back');
-    }).catch(function(error){
+    }).catch(function (error) {
         console.log("Error in creating the post ");
         return;
+    })
+}
+
+//deleting a post
+module.exports.destroy = function (request, response) {
+    Post.findById(request.params.id).then(function (post) {
+        //.id means converting the object id into String
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({ post: request.params.id }).then().catch(function (error) {
+                console.log(error);
+                return response.redirect('back');
+            })
+        }
+    }).catch(function (error) {
+        console.log(error);
+        return response.redirect('back');
     })
 }
